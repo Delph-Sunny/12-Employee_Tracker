79b84da0275febae55d8444337d1c86c632d4477
Delph <delph_teb@hotmail.com>
2021-01-28 01:32:55 -0500

    fix typo in sql

--- seeds.sql
+++ seeds.sql
@@ -27,17 +27,17 @@ INSERT INTO role (title, salary, department_id)
 VALUE ("Lawyer", 190000, 3);
 
 /* EMPLOYEE SEEDS */
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUE ("John", "Doe", 1, 3);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUE ("Mike", "Chan", 2, 1);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUE ("Ashley", "Rodriguez", 3, null);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUE ("Kevin", "Tupik", 4, 3);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUE ("Malia", "Brown", 5, null);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUE ("Sarah", "Lourd", 6, null);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUE ("Tom", "Allen", 7, 6);
